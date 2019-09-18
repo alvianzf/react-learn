624d25dd67816f2d423d474b865e9e651de6e602
Alvian Zachry <alvian.faturrahman@webimp.com.sg>
2019-09-18 14:40:46 +0700

    about page

--- src/Component/About.js
+++ src/Component/About.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
 import { Container, Row, Col, Carousel } from 'react-bootstrap'
-import Isi2 from '../Asset/images/isi2.jpg'
 
 import MeetSolv from './About/MeetSolv'
+import History from './About/History'
 
 class About extends Component {
     render() {
         return (
-            <MeetSolv />
+            <Container>
+                <Row  className="cards">
+                    <MeetSolv />
+                </Row>
+                <Row className="cards">
+                    <History />
+                </Row>
+            </Container>
         )
     }
 }
---
+++ src/Component/About/History.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+import Isi3 from '../../Asset/images/isi3.jpg'
+
+
+class History extends Component {
+    render() {
+        return (
+            <Container>
+                <Row>
+                    <Col md={3}>
+                        <img width="100%" className="card-image" alt="isi3" src={ Isi3 } />
+                    </Col>
+                    <Col md="9">
+                        <h3>History</h3>
+                        <p className="article">
+                            Gojek’s journey began in 2010 as a motorcycle ride-hailing call center in Indonesia. The homegrown app was then launched in 2015 with only three services: GoRide, GoSend, and GoMart.
+                            <br />
+                            Since then, the app has evolved into a Super App, a multi-services platform with more than 20 services today.
+                            <br />
+                            Gojek is now a leading technology group of platform serving millions of users in Southeast Asia.
+                        </p>
+                    </Col>
+                </Row>
+            </Container>
+        )
+    }
+}
+
+export default History
