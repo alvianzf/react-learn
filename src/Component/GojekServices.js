894509e3472c0b8a6323637f18c4e465faab4dcd
Alvian Zachry <alvian.faturrahman@webimp.com.sg>
2019-09-18 13:39:18 +0700

    services root

---
+++ src/Component/GojekServices.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+class GojekServices extends Component {
+    render() {
+        return (
+            <>
+            </>
+        )
+    }
+}
+
+export default GojekServices
