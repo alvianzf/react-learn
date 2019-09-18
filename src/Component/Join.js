d336224967271c20cf426dfd9a06e8b8f96beea8
Alvian Zachry <alvian.faturrahman@webimp.com.sg>
2019-09-18 12:50:29 +0700

    init

---
+++ src/Component/Join.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+class Join extends Component {
+    render() {
+        return (
+            <Container>
+                <Row>
+                    <Col>This Col</Col>
+                </Row>
+            </Container>
+        )
+    }
+}
+
+export default Join
---
+++ src/Component/Navbar.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react"
+import { Navbar, Nav, NavDropdown } from "react-bootstrap"
+import Logo from '../Asset/images/gojek-logo_normal.svg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGlobe } from '@fortawesome/free-solid-svg-icons'
+
+import Services from './Services'
+import Join from './Join'
+// import { Link } from "react-router-dom"
+
+import "./App.scss"
+
+class NavBar extends Component {
+
+  render() {
+
+    return (
+      <Navbar bg="white" static="top">
+        <Navbar.Brand href="#home"><img alt="gojek-logo" src={ Logo } /></Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">About Us</Nav.Link>
+          <NavDropdown title="Services" id="collasible-nav-dropdown">
+            <Services />
+          </NavDropdown>
+          <Nav.Link href="#features">Blog</Nav.Link>
+          <Nav.Link href="#pricing">Help Centre</Nav.Link>
+          <NavDropdown title="Join Us" id="collasible-nav-dropdown">
+            <Join />
+          </NavDropdown>
+          <Nav.Link>Cerdikiawan</Nav.Link>
+        </Nav>
+
+        <NavDropdown title={
+            (<FontAwesomeIcon icon={faGlobe} > English </FontAwesomeIcon>)}>
+          <NavDropdown.Item>
+            Indonesian
+          </NavDropdown.Item>
+        </NavDropdown>
+      </Navbar>
+    )
+  }
+}
+export default NavBar
