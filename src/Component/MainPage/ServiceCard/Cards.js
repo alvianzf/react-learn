b61a2498e16ed8d72a063eeda8063b8b20980253
Alvian Zachry <alvian.faturrahman@webimp.com.sg>
2019-09-19 14:47:41 +0700

    Fixed issues

--- src/Component/MainPage/ServiceCard/Cards.js
+++ src/Component/MainPage/ServiceCard/Cards.js
@@ -1,24 +1,26 @@
 import React, { Component } from "react"
-import { Card } from "react-bootstrap"
+import { Card, Button } from "react-bootstrap"
 
 class Cards extends Component {
   render() {
     return (
-      <Card
-        className="box"
-        bg="white"
-        style={{ height: "18rem", width: "19rem" }}
-      >
-        <Card.Body className="image-center">
-          <img src={this.props.image} alt="logo" />
-          <Card.Title className="goride-title">
-            <h3>
-              <b>{this.props.title}</b>
-            </h3>
-          </Card.Title>
-          <Card.Text className="article">{this.props.description}</Card.Text>
-        </Card.Body>
-      </Card>
+      <Button variant="white">
+          <Card
+            className="box"
+            bg="white"
+            style={{ height: "18rem", width: "19rem" }}
+          >
+            <Card.Body className="image-center">
+              <img src={this.props.image} alt="logo" />
+              <Card.Title className="goride-title">
+                <h3>
+                  <b>{this.props.title}</b>
+                </h3>
+              </Card.Title>
+              <Card.Text className="article">{this.props.description}</Card.Text>
+            </Card.Body>
+          </Card>
+      </Button>
     )
   }
 }
