e8744cd9409197e3534430d2cd2e2ae179a0e2ac
bianca <belindabee42@gmail.com>
2019-09-21 00:55:11 +0700

    button box flex in home gojek page

--- src/Component/MainPage/GojekServices.js
+++ src/Component/MainPage/GojekServices.js
@@ -10,6 +10,15 @@ import logo5 from "../../Asset/images/iconmerah1.png"
 import logo6 from "../../Asset/images/iconmerah2.png"
 import logo7 from "../../Asset/images/iconmerah3.png"
 import logo8 from "../../Asset/images/iconmerah4.svg"
+import logo9 from "../../Asset/images/iconbiru1.png"
+import logo10 from "../../Asset/images/iconbiru2.png"
+import logo11 from "../../Asset/images/iconbiru3.png"
+import logo12 from "../../Asset/images/iconbiru4.png"
+import logo13 from "../../Asset/images/iconbiru5.svg"
+import logo14 from "../../Asset/images/iconkuning1.png"
+import logo15 from "../../Asset/images/iconkuning2.png"
+import logo16 from "../../Asset/images/iconkuning3.png"
+import logo17 from "../../Asset/images/iconkuning4.png"
 
 class GojekServices extends Component {
   render() {
@@ -93,8 +102,62 @@ class GojekServices extends Component {
               </p>
             </Row>
             <Row>
-              <Cards />
+              <Cards
+                image={logo9}
+                title="gopay"
+                description="LIhat cara top up Gopay (isi saldo) via driver, Alfamart, bank BCA, BRI, Mandiri, BNI, dan lainnya di sini. LIhat juga cara cek saldo Gopay di sini"
+              />
+              <Cards
+                image={logo10}
+                title="gobills"
+                description="Bayar tagihan listrik, BPJS, gas, air, tv kabel, internet, beli token listrik. Tanpa antri dan bisa pakai Gopay, bayar tagihan apapun di GoBills, aja"
+              />
+              <Cards
+                image={logo11}
+                title="gopoints"
+                description="Receive a token for each transaction, swipe them to earn points, and get amazing rewards."
+              />
+              <Cards
+                image={logo12}
+                title="paylater"
+                description="Order any Gojek service or from any Gojek partner and pay at the end of the month with PayLater"
+              />
+              <Cards
+                image={logo13}
+                title="gopulsa"
+                description="it's now easier to top up your phone credit and buy a data package with Gopulsa. Just usr Gopay in Gojek app."
+              />
             </Row>
+            <Row>
+              <h2>Daily Needs</h2>
+            </Row>
+            <Row>
+              <p>
+                Those regular chores that call for your attenction? we have all
+                products under daily needs to make your life simpler and
+                hassle-free.
+              </p>
+            </Row>
+            <Cards
+              image={logo14}
+              title="gomessage"
+              description="Recharge your body and experience your very own version of on demand message!"
+            />
+            <Cards
+              image={logo15}
+              title="goclean"
+              description="Proprize what you & leave all house-cleaning duties to GoClean!"
+            />
+            <Cards
+              image={logo16}
+              title="goauto"
+              description="Order GoAuto for auto care service, towing & emergency services, anytime and anywhere. All available from the GoLife app."
+            />
+            <Cards
+              image={logo17}
+              title="goglam"
+              description="with GoGlam, pamper yoursself with beauty services such as manipadei, hair cream bath or waxing, from the comfort of your own home."
+            />
           </CardDeck>
         </Row>
       </Container>
